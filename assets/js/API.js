c96ad55f26c6ae5ecdc20662cb9504749d07d589
Carlos <carlosaac232001@gmail.com>
2025-08-27 15:21:59 -0500

    feat(API): add newClient function to add client by POST method and getCLients by GET method

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,5 +1,26 @@
 const URL = 'http://localhost:4000/clientes';
 
-export function newClient(client) {
-  console.log(client);
+export async function newClient(client) {
+  try {
+    await fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify(client),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    window.location.href = 'index.html';
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getClients() {
+  try {
+    const res = await fetch(URL);
+    const clients = await res.json();
+    return clients;
+  } catch (error) {
+    console.error(error);
+  }
 }
