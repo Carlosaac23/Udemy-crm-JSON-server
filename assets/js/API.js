423159bb026aff62f71a38b44e49dcc17d4aa8d5
Carlos <carlosaac232001@gmail.com>
2025-08-27 15:56:41 -0500

    feat(API): add deleteClient to delete a client by id and getClient to get client by id and edit it

--- assets/js/API.js
+++ assets/js/API.js
@@ -1,5 +1,6 @@
 const URL = 'http://localhost:4000/clientes';
 
+// Add client
 export async function newClient(client) {
   try {
     await fetch(URL, {
@@ -15,11 +16,34 @@ export async function newClient(client) {
   }
 }
 
+// Get clients
 export async function getClients() {
   try {
     const res = await fetch(URL);
     const clients = await res.json();
     return clients;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Delete client
+export async function deleteClient(id) {
+  try {
+    await fetch(`${URL}/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Get client by ID
+export async function getClient(id) {
+  try {
+    const res = await fetch(`${URL}/${id}`);
+    const client = await res.json();
+    return client;
   } catch (error) {
     console.error(error);
   }
